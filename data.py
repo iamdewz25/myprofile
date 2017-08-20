2533aacd044babe3aa6a458d7901f56bb652dbf6
Nawis Satjadee <iamdewz25@gmail.com>
2017-08-20 15:46:00 +0700

    eiei

--- data.py
+++ data.py
@@ -1,14 +1,14 @@
 data = {
-    'name': 'James Tiberius Kirk',
-    'nickname': 'Jim',
-    'image': 'kirk.jpg',
+    'name': 'NOOB',
+    'nickname': 'Noob',
+    'image': 'troll-slap-rage-smiley-emoticon.gif',
     'greeting_msg': 'Hi, there!',
     'about': """
-        James Tiberius "Jim" Kirk was a male Human Starfleet officer in the 23rd century. He was arguably one of the most famous and highly decorated starship captains in the history of Starfleet. As the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, Kirk served United Federation of Planets interests as an explorer, soldier, time-traveler, and diplomat.
+        Noob
     """,
     'work': {
-        'position': 'Starfleet officer',
-        'at': 'Starfleet'
+        'position': 'Noob',
+        'at': 'Noob'
     },
     'courses': [
         {
@@ -33,11 +33,11 @@ data = {
         }
     ],
     'skills': [
-        'Leadership',
-        'Leadership',
-        'Human Language',
-        'Borg Language',
-        'Valcan Language',
-        'Sleeping'
+        'Noob',
+        'Noob',
+        'Noob',
+        'Noob',
+        'Noob',
+        'Noob'
     ]
 }
